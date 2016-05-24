1c09076ea282e4ffba75460e6e2b16d6ce680938
Gijs van Til <gijsvantil@gmail.com>
2016-05-24 15:48:44 +0200

    created a simple get request in app.js and added html structure to index.pug

---
+++ app.js
@@ -0,0 +1,26 @@
+//////////////////////////////////////////////////
+/// BULLETIN BOARD APPLICATION
+//////////////////////////////////////////////////
+
+//requering
+var express = require ('express')
+
+var app = express()
+
+// View engine = PUG
+app.set('view engine', 'pug');
+app.set('views', './views'); 
+
+
+app.get ('/', function (req, res){
+	console.log('received a get request')
+	res.render('index')
+});
+
+
+
+
+// server set up
+var server = app.listen(3000, function () { 
+	console.log('My Bulletin Board Application is listening on port: ' + server.address().port);
+});
