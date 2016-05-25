43b398a3ffe6876e58bb70433f801e547afc2922
Gijs van Til <gijsvantil@gmail.com>
2016-05-25 18:08:14 +0200

    set up database messages and connected the POST request to it

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 //////////////////////////////////////////////////
 
 //requering
-
 var express = require ('express')
 var app = express()
+
 var pg = require ('pg')
-var connectionString = 
+var connectionString = "postgres://pgadmin:pwdaccess@localhost/bulletinboard"
 
 
 app.use(express.static('./resources/'));
@@ -24,8 +24,18 @@ app.get ('/', function (req, res){
 
 app.post ('/submitbulletin', function (req, res){
 	console.log('received a post request')
-
-})
+	pg.connect(connectionString, function (err, client, done){
+		client.query ('insert into messages (title, body) values (\'Hello World\', \'This is a third test message\')', function (err){
+			if (err){
+				console.log ('Apparently something went bloody wrong' + err)
+			}
+		done();
+		pg.end();
+		console.log ('a message has been added to the bulletinboard')
+		});
+	});
+	res.redirect ('succes')
+});
 
 
 // server set up
